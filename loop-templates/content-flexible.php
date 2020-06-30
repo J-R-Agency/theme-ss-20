5a93a92a4b0cec921e6bb3632bc00568e2d963a7
Joy Ling <joy@Joys-MacBook-Pro.local>
2020-06-30 12:57:51 +0100

    Responsive adjustments

--- loop-templates/content-flexible.php
+++ loop-templates/content-flexible.php
@@ -96,6 +96,7 @@ if( have_rows('flexible_content_block') ):
 			$fcis_columns = get_sub_field('fcis_columns');
 			$fcis_title = get_sub_field('fcis_title');
 			$fcis_subtitle = get_sub_field('fcis_subtitle');
+			$fcis_icon_type = get_sub_field('fcis_icon_type');
 			$fcis_icons = get_sub_field('fcis_icons');
 			$fcis_button = get_sub_field('fcis_button');
 			
@@ -124,7 +125,7 @@ if( have_rows('flexible_content_block') ):
 					$fcis_icon_description = get_sub_field('fcis_icon_description');
 					
 					echo "
-					<div class='fcis-wrapper column-".$fcis_columns."'>
+					<div class='fcis-wrapper column-".$fcis_columns." ".$fcis_icon_type."'>
 						<img src='".$fcis_icon['url']."' alt='".$fcis_icon['alt']."'>";
 						
 					if ($fcis_icon_title) {

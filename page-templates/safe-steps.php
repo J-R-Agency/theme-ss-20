22cb4ddca7ff4b14d83dd65ce65366006c2f95da
Joy Ling <joy@joys-mbp.home>
2020-07-01 10:17:21 +0100

    Added Safe Steps intro block

--- page-templates/safe-steps.php
+++ page-templates/safe-steps.php
@@ -11,9 +11,77 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+$theme_path = get_template_directory_uri();
+
 get_header(); ?>
 
 <?php include_once (get_template_directory() . '/global-templates/template-parts/page-intro.php'); ?>
+
+
+<?php
+	$ss_title = get_field('ss_title');
+	$ss_images = get_field('ss_images');
+	$ss_secondary_list_title = get_field('ss_secondary_list_title');
+?>
+
+<?php if ($ss_title):?>
+	<section class='generic bg-white safe-steps-intro'>
+		
+		<div class='ss-container-top'>
+			<div class='ss-container-left'>
+				<img class='ss-image left'
+					 src='<?php echo $ss_images['ss_image_left']['url']; ?>'
+					 alt='<?php echo $ss_images['ss_image_left']['alt']; ?>'>			
+			</div>
+			
+			<div class='ss-container-center'>
+				
+				<h2><?php echo $ss_title; ?></h2>
+				
+				<img class='ss-image primary'
+					 src='<?php echo $ss_images['ss_primary_image']['url']; ?>'
+					 alt='<?php echo $ss_images['ss_primary_image']['alt']; ?>'>
+				
+				<?php if( have_rows('ss_primary_list') ): ?>
+					<ul>
+					<?php while( have_rows('ss_primary_list') ) : the_row();
+						$ss_primary_list_item = get_sub_field('ss_primary_list_item');
+					?>
+					
+						<li><?php echo $ss_primary_list_item; ?></li>
+					<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+				
+			</div>
+			
+			<div class='ss-container-right'>
+				<img class='ss-image right'
+					 src='<?php echo $ss_images['ss_image_right']['url']; ?>'
+					 alt='<?php echo $ss_images['ss_image_right']['alt']; ?>'>				
+			</div>
+		</div>
+		
+		<div class='generic ss-container-bottom'>
+			<h2><?php echo $ss_secondary_list_title; ?></h2>
+			
+			<?php if( have_rows('ss_secondary_list') ): ?>
+				<div class='secondary-list'>
+				<?php while( have_rows('ss_secondary_list') ) : the_row();
+					$ss_primary_list_item = get_sub_field('ss_secondary_list_item');
+				?>
+					<div class='secondary-list-item'>
+						<img src='<?php echo $theme_path; ?>/assets/icons/checkmark-teal.png'>
+						<p><?php echo $ss_primary_list_item; ?></p>
+					</div>
+				<?php endwhile; ?>
+				</div>
+			<?php endif; ?>			
+			
+		</div>
+		
+	</section>
+<?php endif; ?>
 
 <?php get_template_part( 'loop-templates/content', 'flexible' ); ?>
 

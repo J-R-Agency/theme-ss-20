864e050ccc2a605908c6d7882e40621be5a56efb
Greg Macoy <greg.macoy@jnragency.co.uk>
2020-08-04 00:09:33 +0100

    Update Map with new link

--- loop-templates/content-flexible.php
+++ loop-templates/content-flexible.php
@@ -575,7 +575,7 @@ if( have_rows('flexible_content_block') ):
        		<!-- Company Info Block -->
    			<section class='generic-lg bg-teal'>
    				<div class='company-info-block generic-wrapper'>
-	   				<iframe src='https://www.google.co.uk/maps/place/Safe+Steps/@53.4038331,-2.9719052,17z/data=!3m1!4b1!4m5!3m4!1s0x487b211ae153ada9:0xb6a1a59bf543d7a8!8m2!3d53.4038331!4d-2.9697165' width='600' height='450' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>   
+	   				<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2378.6146371733503!2d-2.9719051841593815!3d53.40383307999107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487b211ae153ada9%3A0xb6a1a59bf543d7a8!2sSafe%20Steps!5e0!3m2!1sen!2suk!4v1596496122182!5m2!1sen!2suk' width='600' height='450' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>   
 	   							
 	   				<div class='company-info-wrapper'>
 	   					<p class='company-phone'>".$phone_number."</p>";
